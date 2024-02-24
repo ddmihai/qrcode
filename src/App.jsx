24979384c34df9304e7f4ce80be4b8e89c0c106c
daniel mihai <sasdaniel9@gmail.com>
2024-02-24 13:34:30 +0000

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <div className={styles.figureImage}>
           <img src={qrCode} alt='Check QR' className={styles.figureImageComponent}  />
         </div>
-        {/* aside */}
+        {/* asides */}
         <aside className={styles.textWrapper}>
           <h1>Improve your front-end skills by building projects</h1>
           <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
